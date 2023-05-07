fdd12573fde83c1a20f82eeee242a3fa6b80d82e
Izhar J <eezhar@gmail.com>
2023-05-07 18:58:29 +0800

    ts-cart proj file init

---
+++ ts-react-hooks/src/App.tsx
@@ -0,0 +1,16 @@
+import {useState} from "react";
+
+interface User {
+    id: number,
+    username: string
+}
+function App() {
+  const [count, setCount] = useState<number>(0)
+    const [users, setUsers] = useState<User[] | null>(null)
+
+  return (
+      <></>
+  )
+}
+
+export default App
